b9d4d58674b7e2ff01b0e9c0d59b4fa79ff93e52
louei <djloueigenita@gmail.com>
2022-07-03 12:38:28 +0800

    first commit

---
+++ database/seeders/customerSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Customer;
+
+class customerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    // public function run()
+    // {
+    //     DB::table('customers')->insert(
+    //         [
+    //             [
+    //                 'name' => 'Myrwen Dingal',
+    //                 'contactNo' => '0985155',
+    //                 'address' => 'CDO, Bukidnon', 
+    //             ],
+    //             [
+    //                 'name' => 'Robert',
+    //                 'contactNo' => '02323656',
+    //                 'address' => 'Negros Occidental', 
+    //             ]
+    //         ]
+    //             );
+    // }
+        public function run(){
+            // factory(Customer::class, 6)->create();
+    }
+}
